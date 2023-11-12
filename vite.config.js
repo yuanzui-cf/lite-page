f5492a5f30f21314bf2ff601eb5b59f4c2f5cea1
yuanzui-cf <grassblogstu@gmail.com>
2023-11-12 17:32:31 +0800

    Remove useless files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-
-export default {
-  plugins: [
-    ViteImageOptimizer()
-  ],
-};
