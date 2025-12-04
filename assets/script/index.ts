c1083b82b881b3e25a44d2aa45bd9b47df892bcb
yuanzui-cf <grassblogstu@gmail.com>
2025-12-04 14:45:15 +0800

    style: Reformat TypeScript and tidy code

--- assets/script/index.ts
+++ assets/script/index.ts
@@ -1,28 +1,29 @@
-import { litePage } from "./page.ts"
+import { litePage } from "./page.ts";
 const page = new litePage({
-    githubID: "yuanzui-cf"
-})
+  githubID: "yuanzui-cf",
+});
+
 page.setLink([
-    {
-        id: "github",
-        link: "https://github.com/yuanzui-cf",
-    },
-    {
-        id: "mail",
-        link: "mailto:grassblogstu@gmail.com",
-    },
-    {
-        id: "blog",
-        link: "https://blog.yzcf.top",
-    },
-    {
-        id: "support",
-        link: "#donate",
-    },
-    {
-        id: "telegram",
-        link: "https://t.me/yz_cf",
-    },
-])
-page.getPosts("https://blog.yzcf.top/feed.xml")
-page.setProject()
+  {
+    id: "github",
+    link: "https://github.com/yuanzui-cf",
+  },
+  {
+    id: "mail",
+    link: "mailto:im.leojia@gmail.com",
+  },
+  {
+    id: "blog",
+    link: "https://blog.yzcf.top",
+  },
+  {
+    id: "support",
+    link: "#donate",
+  },
+  {
+    id: "telegram",
+    link: "https://t.me/kami_madoka",
+  },
+]);
+page.getPosts("https://blog.yzcf.top/feed.xml");
+page.setProject();
